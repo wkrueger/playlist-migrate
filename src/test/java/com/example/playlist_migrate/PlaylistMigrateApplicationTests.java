4c917e8fe05b0fbbaaaf35ebe9f8b70b571f809b
Willian Krueger <wkrueger128@gmail.com>
2025-02-01 00:16:37 -0300

    init

---
+++ src/test/java/com/example/playlist_migrate/PlaylistMigrateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.playlist_migrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistMigrateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
