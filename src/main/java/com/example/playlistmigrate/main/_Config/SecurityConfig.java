8f722403c97dc11d1292c6249aa3ba3b1e496ca3
Willian Krueger <wkrueger128@gmail.com>
2025-02-05 11:03:41 -0300

    teste user

--- src/main/java/com/example/playlistmigrate/main/_Config/SecurityConfig.java
+++ src/main/java/com/example/playlistmigrate/main/_Config/SecurityConfig.java
@@ -32,19 +32,15 @@ public class SecurityConfig {
 
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http
-        .csrf(csrf -> csrf.disable()) // Disable CSRF for stateless APIs
-        .authorizeHttpRequests(auth -> auth
-            .requestMatchers("/auth/welcome", "/auth/addNewUser", "/auth/generateToken").permitAll()
-            .requestMatchers("/auth/user/**").hasAuthority("ROLE_USER")
-            .requestMatchers("/auth/admin/**").hasAuthority("ROLE_ADMIN")
-            .anyRequest().authenticated() // Protect all other endpoints
-        )
-        .sessionManagement(sess -> sess
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // No sessions
-        )
-        .authenticationProvider(authenticationProvider()) // Custom authentication provider
-        .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class); // Add JWT filter
+    http.csrf(csrf -> csrf.disable()) // Disable CSRF for stateless APIs
+        .authorizeHttpRequests(
+            auth -> auth.requestMatchers("/auth/welcome", "/auth/addNewUser", "/auth/generateToken")
+                .permitAll().requestMatchers("/auth/user/**").hasAuthority("ROLE_USER")
+                .requestMatchers("/auth/admin/**").hasAuthority("ROLE_ADMIN")
+                .requestMatchers("/actuator/**").permitAll().anyRequest().authenticated())
+        .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authenticationProvider(authenticationProvider())
+        .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class);
 
     return http.build();
   }
